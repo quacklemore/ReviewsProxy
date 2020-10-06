65412880df703ee8ba9dd12400f06d7e2c7fe107
root <tak074@ucsd.edu>
2020-10-06 10:53:12 -0700

    the rest of the api requests

--- server/index.js
+++ server/index.js
@@ -11,9 +11,10 @@ app.listen(port, function() {
   console.log(`listening on port: ${port}`);
 });
 
-app.get('/api/low-days/0', (req, res) => {
+app.get('/api/low-days/:id', (req, res) => {
+  let lowId = req.params.id;
   axios({
-    url: 'http://localhost:4002/api/low-days/0',
+    url: `http://localhost:4002/api/low-days/${lowId}`,
     method: 'GET'
   })
   .then(result => {
@@ -24,8 +25,37 @@ app.get('/api/low-days/0', (req, res) => {
   });
 });
 
+app.get('/api/trips/:id', (req, res) => {
+  let tripId = req.params.id;
+  axios({
+    url: `http://localhost:4002/api/low-days/${tripId}`,
+    method: 'GET'
+  })
+  .then(result => {
+    res.send(result.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+app.post('/api/trips/', (req, res) => {
+  axios({
+    url: 'http://localhost:4000/api/trips/',
+    method: 'POST',
+    data: {
+      "trip": req.body.trip
+    }
+  })
+  .then(result => {
+    res.send(result.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
 app.post('/api/pictures/', (req, res) => {
-  console.log('api/pictures');
   axios({
     url: 'http://localhost:4000/api/pictures/',
     method: 'POST',
@@ -39,7 +69,6 @@ app.post('/api/pictures/', (req, res) => {
   .catch(err => {
     console.log(err);
   });
-
 });
 
 app.get('/api/hotel/:hotelId', (req, res) => {
